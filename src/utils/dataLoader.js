90d521f5553e612b110a9cc98c2981d9ee2b7af1
Carlos Perilla <cperilla@users.noreply.github.com>
2025-10-01 18:13:35 -0500

    Q4 Calendar (#29)
    * Update the calendar with sage events
    
    * Update the theme
    
    ---------
    
    Co-authored-by: Carlos Perilla <carlos.perilla@valkertown.org>

--- src/utils/dataLoader.js
+++ src/utils/dataLoader.js
@@ -1,4 +1,4 @@
-import eventsData from '../data/Eventos_Comunidad_Q3_2025.json';
+import eventsData from '../data/Eventos_Comunidad_Q4_2025.json';
 
 export const loadEventsData = () => {
   return eventsData;
