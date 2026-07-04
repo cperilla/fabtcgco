35fdf1bccb98d1da3247ee6e5d21d3975923c526
Carlos Perilla <carlos.perilla@valkertown.org>
2026-07-04 12:04:36 -0500

    Build static site for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,10 @@
 // @ts-check
-import { defineConfig, sessionDrivers } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  session: {
-    driver: sessionDrivers.lruCache()
-  },
-  adapter: cloudflare({
-    prerenderEnvironment: 'node',
-    platformProxy: {
-      enabled: true
-    }
-  }),
-
   vite: {
     plugins: [tailwindcss()]
   },
